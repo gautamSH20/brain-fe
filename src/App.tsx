3df06c8fe48fb921473d658a9a8ce879e6837434
gautamSH20 <gs841400@gmail.com>
2025-01-16 20:59:37 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { SignIn } from "./pages/Sigin";
 import { SignUp } from "./pages/SiginUp";
 import { DashBoard } from "./pages/DashBoard";
+import { Share } from "./pages/Share";
+import { Home } from "./pages/home";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/:share" element={<Share />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
